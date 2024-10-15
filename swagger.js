d29306c3467fcd67d9b66670e3099105cc0d1569
Kanishk <kanishkas466@gmail.com>
2024-10-15 18:04:02 +0530

    update swagger.js

--- swagger.js
+++ swagger.js
@@ -6,13 +6,11 @@ const doc = {
     description: 'Description'
   },
   host: 'localhost:5000'
-};
+};  
 
 const outputFile = './swagger-output.json'; 
 const routes = ['./src/routes/authRoutes.js' , './src/routes/appointmentRoutes.js' , './src/routes/doctor.route.js' , './src/routes/patient.route.js'];
 
 
 
-swaggerAutogen()(outputFile, routes, doc).then(()=>{
-  import('./server.js');
-});
+swaggerAutogen()(outputFile, routes, doc);
